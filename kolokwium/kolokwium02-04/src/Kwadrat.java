2f19d686718cd705d6e40165166989a3fc2f8233
Jakub Więcek <wieza@MacBook-Air-Jakub.local>
2022-04-02 16:47:22 +0200

    s24279-02.04

---
+++ kolokwium/kolokwium02-04/src/Kwadrat.java
@@ -0,0 +1,20 @@
+public class Kwadrat extends Prostokat {
+
+    public Kwadrat(double bok1, double bok11) {
+        super(bok1, bok11);
+    }
+
+    public Kwadrat(double bok1) {
+        super(bok1, bok1);
+    }
+
+    @Override
+    public double przekatna(double v) {
+        return super.przekatna(v);
+    }
+
+    public boolean equals(Prostokat Kwadrat) {
+        return true;
+    }
+
+}
---
+++ kolokwium/kolokwium02-04/src/Prostokat.java
@@ -0,0 +1,11 @@
+public class Prostokat {
+    public Prostokat(double bok1, double bok2) {
+
+    }
+
+    public double przekatna(double v) {
+        return v;
+    }
+
+
+}
